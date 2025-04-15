4580b1190012952e317b91d2f31ab23268ed77d0
IkariTyux <git@rph.red>
2025-04-15 18:18:27 +0200

    Cleaning before migration

--- polybar/trayer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-killall trayer || trayer --align right --edge top --monitor primary --expand false --transparent true --SetDockType false --distancefrom top --distance 40 --width 10 
--- polybar/wg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-IP=$(ip a sh wg0 2> /dev/null | grep inet | awk -v FS=' ' '{print $2}')
-
-if [[ $IP = '10.100.0.1/32' ]]; then
-    echo "%{F#ED8796}󰴴 "
-else
-    echo "%{F#707880}󰴴 "
-fi
